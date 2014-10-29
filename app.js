ce6fb1facdd50e9b03365b746bb68bba33370d3e
Tbomas Beutler <tcbeutler@gmail.com>
2014-10-28 19:03:54 -0500

    Complete! Running kickball form fill

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var encode = require('form-urlencoded').encode;
 var cheerio = require('cheerio');
 var chance = new require('chance')();
 
+var fillForm = require('./fillKickballForm');
+
 var i = 0;
 
 fire();
@@ -14,9 +16,9 @@ function fire() {
   .then(getFormData)
   .then(encodeForm)
   .delay(randomDelay(4, 20))
-  .then(postForm)
-  .then(function(x){ console.log(i); i++; })
-  .delay(randomDelay(30, 120))
+  //.then(postForm)
+  .then(function(x){ console.log('POSTING FORM ' + i); i++; })
+  //.delay(randomDelay(15, 30))
   .then(fire)
   .fail(function(err) { console.log(err.stack); });
 }
@@ -40,7 +42,15 @@ function getSurvey() {
     res.on('end', function() {
       deferred.resolve(data);
     });
+    res.on('error', function(err) {
+      console.log(err);
+    });
   });
+  
+  req.on('error', function(err) {
+    console.log(err);
+  });
+
   req.end();
   return deferred.promise;
 };
@@ -48,44 +58,45 @@ function getSurvey() {
 function getFormData(page) {
   var $ = cheerio.load(page);
   var form = {};
+  fillForm(form);
 
-  // form.__EVENTVALIDATION =  $('#__EVENTVALIDATION')[0].attribs.value;
-  // form.__VIEWSTATE =  '';
+  form.__EVENTVALIDATION =  $('#__EVENTVALIDATION')[0].attribs.value;
+  form.__VIEWSTATE =  '';
   // form.input_719228758_61_8220005206_0 = 8220005202;
   // form.input_719228758_61_8220020117_0 = 8220005203;
   // form.input_719228758_61_8220005205_0 = 8220005204;
   // form.input_719228758_61_8220005207_0 = 8220020114;
   // form.input_719228758_61_8220020116_0 = 8220020115;
-  // form.NextButton = 'Done';
-  // form.hid_smC0l1d = 'VQ9TXVkmCqk6rBl7dfvUQg_3d_3d';
-  // form.hid_smRsL1d = '';
-  // form.hid_smRs1d = 'E6uK1MhOcpBUysyKlC0vrg_3d_3d';
-  // form.hid_smCSV = '';
-  // form.hid_smS1d = '4xSErAi_2fso5kAHNjabLJHQ_3d_3d';
-  // form.hid_smM0D = 'E6uK1MhOcpBUysyKlC0vrg_3d_3d';
-  // form.hid_smV3Rsn = 'ryjiA1jsXxArHG3rMuiwxg_3d_3d';
-  // form.hid_smS3CT1d = 'PlZjAt3xfOlSQesf0M2E9Q_3d_3d';
-  // form.hid_DC = $('#hid_DC')[0].attribs.value;
-  // form.Hidden_CollectorToken = '';
-  // form.Hidden_Simple = '';
-  // form.hid_l04dez =  $('#hid_l04dez')[0].attribs.value;
-
-  form.__EVENTVALIDATION = $('#__EVENTVALIDATION')[0].attribs.value;
-  form.__VIEWSTATE = '';
-  form.input_719248231_10_0_0 = '8220171630_0';
   form.NextButton = 'Done';
-  form.hid_smC0l1d = 'oAo0Jwe9PDe3aHHLkI371w_3d_3d';
+  form.hid_smC0l1d = 'VQ9TXVkmCqk6rBl7dfvUQg_3d_3d';
   form.hid_smRsL1d = '';
   form.hid_smRs1d = 'E6uK1MhOcpBUysyKlC0vrg_3d_3d';
   form.hid_smCSV = '';
-  form.hid_smS1d = 'sNJEF6uVgsFVlVZA6o_2fc8w_3d_3d';
+  form.hid_smS1d = '4xSErAi_2fso5kAHNjabLJHQ_3d_3d';
   form.hid_smM0D = 'E6uK1MhOcpBUysyKlC0vrg_3d_3d';
-  form.hid_smV3Rsn = 'VW6Yh2TOSWXyxI41272YGQ_3d_3d';
-  form.hid_smS3CT1d = 'NlcVv92lW6m2Zs7czosHUA_3d_3d';
+  form.hid_smV3Rsn = 'ryjiA1jsXxArHG3rMuiwxg_3d_3d';
+  form.hid_smS3CT1d = 'PlZjAt3xfOlSQesf0M2E9Q_3d_3d';
   form.hid_DC = $('#hid_DC')[0].attribs.value;
   form.Hidden_CollectorToken = '';
   form.Hidden_Simple = '';
-  form.hid_l04dez = $('#hid_l04dez')[0].attribs.value;
+  form.hid_l04dez =  $('#hid_l04dez')[0].attribs.value;
+
+  // form.__EVENTVALIDATION = $('#__EVENTVALIDATION')[0].attribs.value;
+  // form.__VIEWSTATE = '';
+  // form.input_719248231_10_0_0 = '8220171630_0';
+  // form.NextButton = 'Done';
+  // form.hid_smC0l1d = 'oAo0Jwe9PDe3aHHLkI371w_3d_3d';
+  // form.hid_smRsL1d = '';
+  // form.hid_smRs1d = 'E6uK1MhOcpBUysyKlC0vrg_3d_3d';
+  // form.hid_smCSV = '';
+  // form.hid_smS1d = 'sNJEF6uVgsFVlVZA6o_2fc8w_3d_3d';
+  // form.hid_smM0D = 'E6uK1MhOcpBUysyKlC0vrg_3d_3d';
+  // form.hid_smV3Rsn = 'VW6Yh2TOSWXyxI41272YGQ_3d_3d';
+  // form.hid_smS3CT1d = 'NlcVv92lW6m2Zs7czosHUA_3d_3d';
+  // form.hid_DC = $('#hid_DC')[0].attribs.value;
+  // form.Hidden_CollectorToken = '';
+  // form.Hidden_Simple = '';
+  // form.hid_l04dez = $('#hid_l04dez')[0].attribs.value;
   return {
     form: form,
     action: $('form#frmS')[0].attribs.action
@@ -93,7 +104,8 @@ function getFormData(page) {
 };
 
 function encodeForm(obj) {
-  obj.form = encode(obj.form);  return obj;
+  obj.form = encode(obj.form);
+  return obj;
 }
 
 function randomDelay(minSeconds, maxSeconds) {
@@ -125,6 +137,13 @@ function postForm(postInfo) {
     res.on('end', function() {
       deferred.resolve(data);
     });
+    res.on('error', function(err) {
+      console.log(err);
+    });
+  });
+
+  req.on('error', function(err) {
+    console.log(err);
   });
 
   req.write(postInfo.form);
---
+++ fillKickballForm.js
@@ -0,0 +1,57 @@
+var chance = new require('chance')();
+
+module.exports = fillKickballForm = function(obj) {
+
+  var allTeams = ['BALLTOWN', 'BLUE', 'TRIPPIN', 'SAVED', 'PARTY'];
+  var otherTeams = ['BLUE', 'TRIPPIN', 'SAVED', 'PARTY']
+  var ourTeam = 'BALLTOWN'
+
+  var rankValues = [
+    8220005202,
+    8220005203,
+    8220005204,
+    8220020114,
+    8220020115
+  ];
+
+  var ids = {
+    'BALLTOWN': 'input_719228758_61_8220005206_0',
+    'BLUE': 'input_719228758_61_8220020117_0',
+    'TRIPPIN': 'input_719228758_61_8220005205_0',
+    'SAVED': 'input_719228758_61_8220005207_0',
+    'PARTY': 'input_719228758_61_8220020116_0'
+  };
+
+  function isFirst(percentFirst) {
+    return (getRandom(0, 100) < percentFirst);
+  }
+
+  function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function orderTeams() {
+    var order;
+    if (isFirst(70)) {
+      order = chance.shuffle(otherTeams);
+      order.unshift(ourTeam);
+    } else {
+      order = chance.shuffle(allTeams)
+    }
+    console.log(order);
+    return order;
+  }
+
+  function assignRanks(teams) {
+    var ranks = {};
+    obj[ids[teams[0]]] = rankValues[0];
+    obj[ids[teams[1]]] = rankValues[1];
+    obj[ids[teams[2]]] = rankValues[2];
+    obj[ids[teams[3]]] = rankValues[3];
+    obj[ids[teams[4]]] = rankValues[4];
+    return ranks;
+  }
+
+  return assignRanks(orderTeams());
+
+}
